a2d63e1c4dd8e6ea5d3395817fc748b42c1b8875
Sukanta Nath <nathsukanta007@gmail.com>
2026-02-27 22:30:05 +0600

    Added error handling to calculator

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,24 @@
-def add(a,b):
-    return a+b
+def add(a, b):
+    return a + b
 
-def subtract(a,b):
-    return a-b
+def subtract(a, b):
+    return a - b
 
-def divide(a,b):
-    return a/b
+def divide(a, b):
+    return a / b
 
+def multiply(a, b):
+    return a * b
+
+
+def calculator(a, b, op):
+    if op == "+":
+        return add(a, b)
+    elif op == "-":
+        return subtract(a, b)
+    elif op == "*":
+        return multiply(a, b)
+    elif op == "/":
+        return divide(a, b)
+    else:
+        return "Invalid operator"
