39219155dab9df3673eee81fa46656e569c2423a
Sukanta Nath <nathsukanta007@gmail.com>
2026-02-27 22:21:58 +0600

    added function to calculate area

--- src/main.py
+++ src/main.py
@@ -1,2 +1,11 @@
 print("My name is Sukanta Nath")
 print("Today is 27 february 2026!")
+
+def area(a,b):
+    a = float(input("Enter base : \n"))
+    b = float(input("Enter height : \n"))
+    area = 0.5 * a * b
+    print(area)
+
+
+area(10,5)
